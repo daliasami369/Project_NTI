4e184a9901ef571cb0e9a870d82f1d444c02b483
Dalia Sami <daliasami369@gmail.com>
2026-08-12 14:27:11 +0300

    Modify complete function to append tasks to completed list
    Updated the complete function to move tasks to a completed list instead of hiding them.

--- projectNTI.js
+++ projectNTI.js
@@ -1,11 +1,13 @@
 function complete(){
 var habitcircle=document.getElementsByClassName("habit");
+var completed=document.getElementsByClassName("completed-list")[0];
 for(var i=0;i<habitcircle.length;i++)
 {
     habitcircle[i].onclick=function()
     {
         var task = this.parentElement;
-            task.style.display = "none";
+           completed.appendChild(task);
+            // task.style.display = "none";
     }
 }
 }
